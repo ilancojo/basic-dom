c613d49bb02541ad21f44b97c214dfdae2c1edc1
ilan cojocaru <117562862+ilancojo@users.noreply.github.com>
2026-06-17 20:31:52 +0300

    adding left and right to  exercise 1

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 // console.log(document)
+
+const ball = document.getElementById("ball")
+
+
+
+
+///right
+const moveRight = function () {
+    let left = parseInt(ball.style.left) || 0
+    left += 15
+    ball.style.left = left + "px"
+}
+///left
+const moveLeft = function () {
+    let left = parseInt(ball.style.left) || 0
+    left -= 15
+    ball.style.left = left + "px"
+}
+
+
+
+
+
+ball.addEventListener("click", moveLeft);
+
+
+document.getElementById("playing-field").appendChild(ball) // now the box will be on the page, and will react to a click!
